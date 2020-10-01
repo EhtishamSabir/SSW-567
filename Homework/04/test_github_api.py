3e37322c45c63204fe1c7ca1d1c074177f592656
Liam Brew <lbrew@stevens.edu>
2020-10-01 01:25:55 -0400

    updated pathing for github api test

--- Homework/04/test_github_api.py
+++ Homework/04/test_github_api.py
@@ -1,21 +1,28 @@
 import unittest
+import sys
+
+sys.path.append("Homework/04/")
+
 from github_api import GitHubAPI
 
 class TestGitHubAPI(unittest.TestCase):
-    
+
     def test_get_repos_quantity(self):
         gh = GitHubAPI("Liam-Brew")
-        self.assertEqual(len(gh.get_repos()), 8, "There should be 8 repos present")
-    
+        self.assertEqual(len(gh.get_repos()), 8,
+                         "There should be 8 repos present")
+
     def test_get_repos_names(self):
         gh = GitHubAPI("Liam-Brew")
-        self.assertEqual(gh.get_repos(), ['CS-135', 'CS-284', 'GitHubApi567', 'SSW-215', 'SSW-322', 'SSW-345', 'SSW-567', 'Triangle567'], ['CS-135', 'CS-284', 'GitHubApi567', 'SSW-215', 'SSW-322', 'SSW-345', 'SSW-567', 'Triangle567'])
+        self.assertEqual(gh.get_repos(), ['CS-135', 'CS-284', 'GitHubApi567', 'SSW-215', 'SSW-322', 'SSW-345', 'SSW-567', 'Triangle567'], [
+                         'CS-135', 'CS-284', 'GitHubApi567', 'SSW-215', 'SSW-322', 'SSW-345', 'SSW-567', 'Triangle567'])
 
     def test_specific_get_commits(self):
         gh = GitHubAPI("Liam-Brew")
         gh.get_repos()
         commits = gh.get_commits()
-        self.assertEqual(commits.get('CS-135'), 6, 'Repo CS-135 should have 6 commits')
+        self.assertEqual(commits.get('CS-135'), 6,
+                         'Repo CS-135 should have 6 commits')
 
     def test_run_status_ok(self):
         gh = GitHubAPI("Liam-Brew")
@@ -24,6 +31,7 @@ class TestGitHubAPI(unittest.TestCase):
     def test_nonexistant_user(self):
         gh1 = GitHubAPI("adwdawwadwwdadwadwwdawadwadddddddddddddddddddd")
         self.assertEqual(gh1.run(), 404, "This user is not on GitHub")
-        
+
+
 if __name__ == "__main__":
     unittest.main()
